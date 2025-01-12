18ad2ea7b5c143c29bab19814c94094c61d3aa2a
dawooddilawar <d.dilawar94@gmail.com>
2025-01-12 02:57:32 +0100

    update CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # Set up CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["https://dawooddilawar.com", "http://localhost:3000"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
